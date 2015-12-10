caa8d07da6d8c0b510601b91df2682ab02f46655
Kustiawanto Halim <kustiawanto.halim@gmail.com>
2015-12-10 18:32:40 +0700

    Update Initial Commit

---
+++ AvrBuild.bat
@@ -0,0 +1,2 @@
+@ECHO OFF
+"C:\Program Files (x86)\Atmel\AVR Tools\AvrAssembler2\avrasm2.exe" -S "C:\Users\kuwali\Documents\NEW_POK\labels.tmp" -fI -W+ie -o "C:\Users\kuwali\Documents\NEW_POK\TugasAkhir.hex" -d "C:\Users\kuwali\Documents\NEW_POK\TugasAkhir.obj" -e "C:\Users\kuwali\Documents\NEW_POK\TugasAkhir.eep" -m "C:\Users\kuwali\Documents\NEW_POK\TugasAkhir.map" "C:\Users\kuwali\Documents\NEW_POK\TugasAkhir.asm"
